2acd3d1b92361d98c6bd4703586b39c40c1936a6
siddhu <siddhu.manka94@gmail.com>
2016-03-02 15:03:35 +0530

    sid commit 2

--- IntegratedFileViewer/src/com/concord/convert/ConvertFile.java
+++ IntegratedFileViewer/src/com/concord/convert/ConvertFile.java
@@ -1,9 +1,11 @@
 package com.concord.convert;
 
+import java.io.IOException;
+
 //rucha made a change
 public interface ConvertFile {
 	String inputPath = "";
 	String outputPath = "";
-	public  String Convert (String inputPath, String outputPath);
+	public  String Convert (String inputPath, String outputPath) throws IOException;
 	
 }
--- IntegratedFileViewer/src/com/concord/convert/ConvertHTML.java
+++ IntegratedFileViewer/src/com/concord/convert/ConvertHTML.java
@@ -1,16 +1,19 @@
 package com.concord.convert;
 
-import org.apache.poi.hssf.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
 public class ConvertHTML implements ConvertFile {
 
 	@Override
-	public String Convert(String inputPath, String outputPath) {
+	public String Convert(String inputPath, String outputPath) throws IOException {
 		int nos;
 		POIFSFileSystem fileSystem = new POIFSFileSystem(new FileInputStream(inputPath));
 		HSSFWorkbook workbook = new HSSFWorkbook(fileSystem);
-		nos= HSSFWorkbook.getNumberOfSheets();
+		nos= workbook.getNumberOfSheets();
 		return outputPath;
 	}
 
