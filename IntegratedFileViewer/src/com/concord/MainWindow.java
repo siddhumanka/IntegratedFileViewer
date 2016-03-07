2ef9922b566b34bdf0ca1a46cb1f57be6ceb033f
rucha <rucha700@gmail.com>
2016-03-07 17:58:13 +0530

    rucha_7/3_5.57

--- IntegratedFileViewer/src/com/concord/MainWindow.java
+++ IntegratedFileViewer/src/com/concord/MainWindow.java
@@ -7,58 +7,57 @@ import com.concord.convert.ConvertHTML;
 import com.concord.convert.ConvertNone;
 import com.concord.convert.ConvertPDF;
 import com.concord.typecheck.TypeCheck;
-public class MainWindow {
-		String inputPath;
-		String outputPath;
-		String type;
-		File input = new File(inputPath);
-		public static void error(){
+public class MainWindow
+{
+		static String inputPath;
+		static String outputPath;
+		static String type;
+		static File input = new File(inputPath);
+		public static void error()
+		{
 			//error handling code
 		}
-	public static void main(String[] args) throws IOException {
-		
-		type = TypeCheck.check(input);
-		
-		switch (type)
+		public static void main(String[] args) throws IOException
 		{
-		case "application/msword":  //doc
-   	    case "application/vnd.openxmlformats-officedocument.wordprocessingml.document": //docx
-   	    case "application/vnd.ms-word.document.macroenabled.12": //docm
-   	    case "application/vnd.oasis.opendocument.text": //odt
-   	    case "application/vnd.ms-powerpoint":   //ppt
-   	    case "application/vnd.openxmlformats-officedocument.presentationml.presentation":  //pptx
-   	    	ConvertPDF convertPDF= new ConvertPDF();
-   	    	outputPath=convertPDF.convert(inputPath);
-   	    	break;
-   	    	//xls, xlsx, csv,ods
-   	 case "text/csv": //csv
-     case "application/vnd.ms-excel": //xls
-	 case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": //xlsx
-	 case "application/vnd.oasis.opendocument.spreadsheet": //ods
-		ConvertHTML convertHTML = new ConvertHTML();
-		outputPath=convertHTML.convert(inputPath);
-		break;
-	 case "application/xhtml+xml": //xhtml
-	 case "text/html":  //html
-	 case "application/json":  //json
-	 case "text/plain":  //txt
-	 case "text/css":  //css
-	 case "application/xml": //xml
-		 //images
-	 case "image/x-ms-bmp": //bmp
-	 case "image/jpeg":  //jpg
-	 case "image/png": //png
-		 ConvertNone convertNone =new ConvertNone();
-		 outputPath=convertNone.convert(inputPath);
-		break;
 		
-	default: 
-		error();
-		
+			type = TypeCheck.check(input);
+			
+			switch (type)
+			{
+			case "application/msword":  //doc
+	   	    case "application/vnd.openxmlformats-officedocument.wordprocessingml.document": //docx
+	   	    case "application/vnd.ms-word.document.macroenabled.12": //docm
+	   	    case "application/vnd.oasis.opendocument.text": //odt
+	   	    case "application/vnd.ms-powerpoint":   //ppt
+	   	    case "application/vnd.openxmlformats-officedocument.presentationml.presentation":  //pptx
+	   	    	ConvertPDF c= new ConvertPDF();
+	   	    	c.Convert(inputPath,outputPath);
+	   	    	break;
+	   	    	//xls, xlsx, csv,ods
+	   	    case "text/csv": //csv
+	   	    case "application/vnd.ms-excel": //xls
+	   	    case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": //xlsx
+	   	    case "application/vnd.oasis.opendocument.spreadsheet": //ods
+	   	    	ConvertHTML h = new ConvertHTML();
+	   	    	h.Convert(inputPath, outputPath);
+	   	    	break;
+	   	    case "application/xhtml+xml": //xhtml
+	   	    case "text/html":  //html
+	   	    case "application/json":  //json
+	   	    case "text/plain":  //txt
+	   	    case "text/css":  //css
+	   	    case "application/xml": //xml
+			 //images
+	   	    case "image/x-ms-bmp": //bmp
+	   	    case "image/jpeg":  //jpg
+	   	    case "image/png": //png
+			 ConvertNone n =new ConvertNone();
+			 n.Convert(inputPath, outputPath);
+			 break;
+			
+	   	    default: 
+	   	    	error();
+			}
 		
 		}
-		
-
-	}
-
 }
